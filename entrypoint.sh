a762c3f688637eb0a478830df18b55ac68bb3383
Chris Pavlicek <chris.pavlicek@velocityapp.com>
2018-07-03 17:58:16 +0100

    Fix docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,6 @@ ln -s $KUBE_PATH/build/docker /usr/local/bin/build-docker
 ln -s $KUBE_PATH/build/circle-app-env /usr/local/bin/build-circle-app-env
 ln -s $KUBE_PATH/deploy/upload-docker-image /usr/local/bin/deploy-upload-docker-image
 ln -s $KUBE_PATH/deploy/kube /usr/local/bin/deploy-kube
-ln -s $KUBE_PATH/deploy/deploy-docker /usr/local/bin/deploy-dockesh -p 64539 18.232.60.29
+ln -s $KUBE_PATH/deploy/deploy-docker /usr/local/bin/deploy-docker
 ln -s $KUBE_PATH/deploy/deploy-to-kube /usr/local/bin/deploy-to-kube
 ln -s $KUBE_PATH/deploy/get-deploy-yaml /usr/local/bin/get-deploy-yaml
