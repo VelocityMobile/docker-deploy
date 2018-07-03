033c49d202fd66189cdaca57fc2bf046071ffbb3
Chris Pavlicek <chris.pavlicek@velocityapp.com>
2018-07-03 13:12:35 +0100

    Fix envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "GOT HERE $@"
-
 while test $# -gt 0; do
   case "$1" in
     -o*|--oauth*)
       shift
-      export OAUTH=$1
+      export KUBE_OAUTH=$1
       shift
       ;;
      -r*|--repo*)
       shift
-      export REPO=$1
+      export KUBE_REPO=$1
       shift
       ;;
     *)
@@ -21,17 +19,14 @@ while test $# -gt 0; do
   esac
 done
 
-echo $OAUTH
-echo $REPO
-
-# Clone using passed in OAUTH token
-git clone --depth 1 -b develop https://$OAUTH:x-oauth-basic@github.com/$REPO.git /usr/lib/$REPO
+# Clone using passed in KUBE_OAUTH token
+git clone --depth 1 -b develop https://$KUBE_OAUTH:x-oauth-basic@github.com/$KUBE_REPO.git /usr/lib/$KUBE_REPO
 
 # Decrypt
-/usr/lib/$REPO/ci-decrypt
+/usr/lib/$KUBE_REPO/ci-decrypt
 
 # Link for easy usage
-ln -s /usr/lib/$REPO/build/docker /usr/local/bin/build-docker
-ln -s /usr/lib/$REPO/build/circle-app-env /usr/local/bin/build-circle-app-env
-ln -s /usr/lib/$REPO/deploy/upload-docker-image /usr/local/bin/deploy-upload-docker-image
-ln -s /usr/lib/$REPO/deploy/kube /usr/local/bin/deploy-kube
+ln -s /usr/lib/$KUBE_REPO/build/docker /usr/local/bin/build-docker
+ln -s /usr/lib/$KUBE_REPO/build/circle-app-env /usr/local/bin/build-circle-app-env
+ln -s /usr/lib/$KUBE_REPO/deploy/upload-docker-image /usr/local/bin/deploy-upload-docker-image
+ln -s /usr/lib/$KUBE_REPO/deploy/kube /usr/local/bin/deploy-kube
